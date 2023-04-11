aac1254724150c40a7c7f09d869b39bffd20ecbb
Matheus Lopes <mlopes.matheus@gmail.com>
2023-04-11 18:38:32 -0300

    chore: fix lint

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vinttem_app/app/app.dart';
-import 'package:vinttem_app/counter/counter.dart';
 import 'package:vinttem_app/home/home.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(App());
+      await tester.pumpWidget(const App());
       expect(find.byType(HomePage), findsOneWidget);
     });
   });

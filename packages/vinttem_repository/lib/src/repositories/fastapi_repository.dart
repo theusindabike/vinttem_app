f4099d9c27efbf31a91aa4b73d694d05e82db5a8
theusindabike <mlopes.matheus@gmail.com>
2023-06-13 14:18:11 -0300

    feat: implement fastAPI createTransaction

--- packages/vinttem_repository/lib/src/repositories/fastapi_repository.dart
+++ packages/vinttem_repository/lib/src/repositories/fastapi_repository.dart
@@ -1,7 +1,5 @@
-import 'package:vinttem_fastapi/vinttem_fastapi.dart' as vinttem_fastapi_client
-    hide Transaction;
-import 'package:vinttem_repository/src/models/transaction.dart';
-import 'package:vinttem_repository/src/repositories/repositories.dart';
+import 'package:vinttem_fastapi/vinttem_fastapi.dart' as vinttem_fastapi_client;
+import 'package:vinttem_repository/vinttem_repository.dart';
 
 class VinttemFastAPIRespository implements VinttemRepository {
   VinttemFastAPIRespository({
@@ -37,7 +35,31 @@ class VinttemFastAPIRespository implements VinttemRepository {
   }
 
   @override
-  Future<Transaction> createTransaction(Transaction transaction) async {
-    return transaction;
+  Future<Transaction> createTransaction(Transaction newTransaction) async {
+    final parsedTransaction = vinttem_fastapi_client.Transaction(
+      id: 1,
+      user: vinttem_fastapi_client.TransactionUser.values
+          .byName(newTransaction.user.name),
+      value: newTransaction.value,
+      category: vinttem_fastapi_client.TransactionCategory.values
+          .byName(newTransaction.category.name),
+      type: vinttem_fastapi_client.TransactionType.values
+          .byName(newTransaction.type.name),
+      description: newTransaction.description,
+    );
+    try {
+      final result =
+          await _vinttemFastAPIClient.createTransaction(parsedTransaction);
+
+      return Transaction(
+        id: result.id,
+        user: TransactionUser.values.byName(result.user.name),
+        value: result.value,
+        category: TransactionCategory.values.byName(result.category.name),
+        type: TransactionType.values.byName(result.type.name),
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 }

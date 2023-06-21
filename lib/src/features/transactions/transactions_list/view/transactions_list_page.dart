162a9ba9678ccf24c0efff5abb88364e4ea5f667
theusindabike <mlopes.matheus@gmail.com>
2023-06-21 10:46:50 -0300

    feat: delete transaction

--- lib/src/features/transactions/transactions_list/view/transactions_list_page.dart
+++ lib/src/features/transactions/transactions_list/view/transactions_list_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vinttem_app/src/features/transactions/transaction.dart';
+import 'package:vinttem_repository/vinttem_repository.dart'
+    as vinttem_repository;
 
 class TransactionsListPage extends StatelessWidget {
   const TransactionsListPage({super.key});
@@ -57,25 +59,46 @@ class TrasactionsListView extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       children: <Widget>[
         for (final t in state.transactions)
-          Card(
-            margin: const EdgeInsets.only(top: 10),
-            child: ListTile(
-              key: Key('transactionCard_${t.id}'),
-              leading: const Icon(Icons.shopping_bag),
-              title: Text(t.category.description),
-              subtitle: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Type: ${t.type.description}'),
-                  Text(
-                    'R\$ ${t.value.toString().replaceAll('.', ',')}',
-                  ),
-                ],
-              ),
-              trailing: const Icon(Icons.more_vert),
+          Dismissible(
+            key: Key('dismissibleTransactionCard_${t.id}'),
+            background: Container(
+              color: Colors.red,
+            ),
+            direction: DismissDirection.endToStart,
+            child: Card(
+              margin: const EdgeInsets.only(top: 10),
+              child: TransactionListTile(transaction: t),
             ),
           )
       ],
+    );
+  }
+}
+
+class TransactionListTile extends StatelessWidget {
+  const TransactionListTile({
+    super.key,
+    required this.transaction,
+  });
+
+  final vinttem_repository.Transaction transaction;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      key: Key('transactionCard_${transaction.id}'),
+      leading: const Icon(Icons.shopping_bag),
+      title: Text(transaction.category.description),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Type: ${transaction.type.description}'),
+          Text(
+            'R\$ ${transaction.value.toString().replaceAll('.', ',')}',
+          ),
+        ],
+      ),
+      trailing: const Icon(Icons.more_vert),
     );
   }
 }

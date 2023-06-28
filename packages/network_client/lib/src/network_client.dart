c40b7fee26131d4f52945983dc13d96a61678632
theusindabike <mlopes.matheus@gmail.com>
2023-06-28 15:15:48 -0300

    refactor: add network typedef

--- packages/network_client/lib/src/network_client.dart
+++ packages/network_client/lib/src/network_client.dart
@@ -1,7 +1,36 @@
-/// {@template network_client}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
+import 'package:http/http.dart' as http;
+
+class NetworkClientError implements Exception {
+  NetworkClientError(this.statusCode, this.message, this.stackTrace);
+
+  final int statusCode;
+  final String message;
+  final StackTrace stackTrace;
+}
+
+typedef PostCall = Future<http.Response> Function(
+  Uri, {
+  Object? body,
+  Map<String, String>? headers,
+});
+
+typedef PutCall = Future<http.Response> Function(
+  Uri, {
+  Object? body,
+  Map<String, String>? headers,
+});
+
+typedef PatchCall = Future<http.Response> Function(
+  Uri, {
+  Object? body,
+  Map<String, String>? headers,
+});
+
+typedef GetCall = Future<http.Response> Function(
+  Uri, {
+  Map<String, String>? headers,
+});
+
 class NetworkClient {
-  /// {@macro network_client}
   const NetworkClient();
 }

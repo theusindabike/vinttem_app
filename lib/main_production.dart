0790db9b98c3bd9953a076710f8310c38b75ba8f
theusindabike <mlopes.matheus@gmail.com>
2023-06-29 16:27:18 -0300

    refactor: use NetworkClient on vinttemFastAPIRepository

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,14 @@
+import 'package:network_client/network_client.dart';
 import 'package:vinttem_app/bootstrap.dart';
 import 'package:vinttem_app/src/app.dart';
 import 'package:vinttem_repository/vinttem_repository.dart';
 
 void main() {
+  final networkClient = NetworkClient(baseUrl: 'https://vinttem.com');
+
   bootstrap(
-    () => App(vinttemRepository: VinttemFastAPIRespository()),
+    () => App(
+      vinttemRepository: VinttemFastAPIRepository(networkClient: networkClient),
+    ),
   );
 }
--- packages/vinttem_repository/lib/src/repositories/repositories.dart
+++ packages/vinttem_repository/lib/src/repositories/repositories.dart
@@ -1,3 +1,3 @@
-export 'fastapi_repository.dart';
 export 'mock_repository.dart';
+export 'vinttem_fastapi_repository.dart';
 export 'vinttem_repository.dart';

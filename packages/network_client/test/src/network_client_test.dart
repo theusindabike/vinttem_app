17d820c6533a536e24c386baac9dbe6b21cd1fb1
theusindabike <mlopes.matheus@gmail.com>
2023-06-28 21:21:15 -0300

    tests: improve coverage

--- packages/network_client/test/src/network_client_test.dart
+++ packages/network_client/test/src/network_client_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:network_client/network_client.dart';
@@ -31,6 +33,7 @@ void main() {
   setUpAll(() {
     registerFallbackValue(Uri.parse('http://localhost'));
   });
+  // ignore: flutter_style_todos, flutter_style_todos
   group('NetworkClient', () {
     const baseUrl = 'http://baseurl.com';
 
@@ -99,6 +102,150 @@ void main() {
         ),
         isNotNull,
       );
+    });
+
+    group('get', () {
+      test('return the response', () async {
+        final response = await networkClient.get('/');
+
+        expect(response.statusCode, expectedResponse.statusCode);
+        expect(response.body, expectedResponse.body);
+      });
+
+      test('sends request with queryParameters correctly', () async {
+        await networkClient.get(
+          '/some/beutifull/endpoint',
+          queryParameters: {
+            'param1': 'value1',
+            'param2': 'value2',
+          },
+        );
+
+        verify(
+          () => mockHttpClient.get(
+            Uri.parse(
+              '$baseUrl/some/beutifull/endpoint?param1=value1&param2=value2',
+            ),
+            headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+          ),
+        ).called(1);
+      });
+    });
+
+    group('post', () {
+      test('return the response', () async {
+        final response = await networkClient.post('/');
+
+        expect(response.statusCode, expectedResponse.statusCode);
+        expect(response.body, expectedResponse.body);
+      });
+
+      test('sends request with queryParameters and body correctly', () async {
+        await networkClient.post(
+          '/some/beutifull/endpoint',
+          queryParameters: {
+            'param1': 'value1',
+            'param2': 'value2',
+          },
+          body: 'SOME_AMAZING_BODY',
+        );
+
+        verify(
+          () => mockHttpClient.post(
+            Uri.parse(
+              '$baseUrl/some/beutifull/endpoint?param1=value1&param2=value2',
+            ),
+            headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+            body: 'SOME_AMAZING_BODY',
+          ),
+        ).called(1);
+      });
+    });
+
+    group('put', () {
+      test('return the response', () async {
+        final response = await networkClient.put('/');
+
+        expect(response.statusCode, expectedResponse.statusCode);
+        expect(response.body, expectedResponse.body);
+      });
+
+      test('sends request with queryParameters and body correctly', () async {
+        await networkClient.put(
+          '/some/beutifull/endpoint',
+          queryParameters: {
+            'param1': 'value1',
+            'param2': 'value2',
+          },
+          body: 'SOME_AMAZING_BODY',
+        );
+
+        verify(
+          () => mockHttpClient.put(
+            Uri.parse(
+              '$baseUrl/some/beutifull/endpoint?param1=value1&param2=value2',
+            ),
+            headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+            body: 'SOME_AMAZING_BODY',
+          ),
+        ).called(1);
+      });
+    });
+
+    group('patch', () {
+      test('return the response', () async {
+        final response = await networkClient.patch('/');
+
+        expect(response.statusCode, expectedResponse.statusCode);
+        expect(response.body, expectedResponse.body);
+      });
+
+      test('sends request with queryParameters and body correctly', () async {
+        await networkClient.patch(
+          '/some/beutifull/endpoint',
+          queryParameters: {
+            'param1': 'value1',
+            'param2': 'value2',
+          },
+          body: 'SOME_AMAZING_BODY',
+        );
+
+        verify(
+          () => mockHttpClient.patch(
+            Uri.parse(
+              '$baseUrl/some/beutifull/endpoint?param1=value1&param2=value2',
+            ),
+            headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+            body: 'SOME_AMAZING_BODY',
+          ),
+        ).called(1);
+      });
+    });
+
+    group('delete', () {
+      test('return the response', () async {
+        final response = await networkClient.delete('/');
+
+        expect(response.statusCode, expectedResponse.statusCode);
+        expect(response.body, expectedResponse.body);
+      });
+
+      test('sends request with queryParameters and body correctly', () async {
+        await networkClient.delete(
+          '/some/beutifull/endpoint',
+          body: 'SOME_AMAZING_BODY',
+        );
+
+        verify(
+          () => mockHttpClient.delete(
+            Uri.parse(
+              '$baseUrl/some/beutifull/endpoint',
+            ),
+            headers: {HttpHeaders.contentTypeHeader: ContentType.json.value},
+            body: 'SOME_AMAZING_BODY',
+          ),
+        ).called(1);
+      });
     });
   });
 }

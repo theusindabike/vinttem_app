e8aa6d8b97eb3c199636eefa56f291d63d147b47
theusindabike <mlopes.matheus@gmail.com>
2023-07-02 21:01:19 -0300

    refactor: create vinttem_ui package

---
+++ packages/vinttem_ui/lib/src/vinttem_ui.dart
@@ -0,0 +1,7 @@
+/// {@template vinttem_ui}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class VinttemUi {
+  /// {@macro vinttem_ui}
+  const VinttemUi();
+}
---
+++ packages/vinttem_ui/lib/vinttem_ui.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library vinttem_ui;
+
+export 'src/vinttem_ui.dart';
